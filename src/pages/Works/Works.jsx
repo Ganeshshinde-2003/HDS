46716f3dd66a6411a5cc3b3d184daf6d1e92faca
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-08-09 11:45:38 +0530

    created the footer

--- src/pages/Works/Works.jsx
+++ src/pages/Works/Works.jsx
@@ -1,13 +1,16 @@
+import AnimatedComponent from "../../components/AnimatedComponent"
 import Card from "./components/Card"
 import Headings from "./components/Headings"
 import styles from "./style.module.css"
 
 function Works() {
   return (
+    <AnimatedComponent>
     <div className={styles.container}>
       <Headings />
       <Card />
     </div>
+    </AnimatedComponent>
   )
 }
 
