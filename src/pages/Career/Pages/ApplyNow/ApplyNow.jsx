02854f132239baa88c40263cab901fb5719df9d5
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-08-16 15:50:31 +0530

    feature: created an api to store the candidate data and intigrated to front end

--- src/pages/Career/Pages/ApplyNow/ApplyNow.jsx
+++ src/pages/Career/Pages/ApplyNow/ApplyNow.jsx
@@ -1,52 +1,139 @@
-import React, { useState } from 'react';
-import styles from './style.module.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./style.module.css";
 
 function ApplyNow() {
-  const [resumeFile, setResumeFile] = useState(null);
-  const [phone, setPhone] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [portfolio, setPortfolio] = useState("");
+  const [resume, setResume] = useState("");
+  const [number, setNumber] = useState("");
+  const [desc, setDesc] = useState("");
+  const [isSuccessAlert, setIsSuccessAlert] = useState(false);
+  const [isErrorAlert, setIsErrorAlert] = useState(false);
+  const [errormsg, setErrormsg] = useState("Submission failed. Please try again.")
 
-  const handleFileChange = (event) => {
-    const selectedFile = event.target.files[0];
-    setResumeFile(selectedFile);
+  const navigate = useNavigate();
+
+  const data = {
+    firstName,
+    lastName,
+    email,
+    number,
+    resume,
+    portfolio,
+    description: desc,
   };
+  const submitData = async (e) => {
+    e.preventDefault();
 
-  const handlePhoneChange = (event) => {
-    const inputPhone = event.target.value;
-    setPhone(inputPhone);
+    try {
+      const response = await fetch("http://localhost:3000/api/application", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        setIsSuccessAlert(true);
+        setTimeout(() => {
+          setIsSuccessAlert(false);
+          setTimeout(() => {
+            navigate("/about");
+          }, 1000);
+        }, 2000);
+      } else {
+        setIsErrorAlert(true)
+        setTimeout(() => {
+          setIsErrorAlert(false);
+        }, 2000);
+      }
+    } catch (error) {
+      setErrormsg("Network Error!")
+    }
   };
 
   return (
     <div className={styles.main}>
       <div className={styles.container}>
-        <form action="">
+        <form>
           <div className={styles.name}>
-            <input type="text" required placeholder="First Name" />
-            <input type="text" required placeholder="Last Name" />
+            <input
+              type="text"
+              required
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input
+              type="text"
+              required
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
           </div>
-          <input type="email" required placeholder="Email" />
+          <input
+            type="email"
+            required
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <input
             type="text"
             required
-            placeholder="Phone"
-            value={phone}
-            onChange={handlePhoneChange}
+            placeholder="Number"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
             pattern="[0-9()-\s]+"
           />
           <div className={styles.resume}>
             <div className={styles.resumename}>Resume/CV</div>
             <input
-              type="file"
-              accept=".pdf,.doc,.docx"
-              onChange={handleFileChange}
-              capture="Upload File"
+              type="text"
+              value={resume}
+              onChange={(e) => setResume(e.target.value)}
+              placeholder="Upload on Google Drive and provide link"
               required
             />
           </div>
-          <input type="text" required placeholder="Portfolio Link" />
-          <textarea type="text" rows="7" cols="10" placeholder='Personal Note' />
-          <button className={styles.button}>SUBMIT APPLICATION</button>
+          <input
+            type="text"
+            required
+            placeholder="Portfolio Link"
+            value={portfolio}
+            onChange={(e) => setPortfolio(e.target.value)}
+          />
+          <textarea
+            rows="7"
+            cols="10"
+            placeholder="Personal Note"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+          <button className={styles.button} onClick={submitData}>
+            SUBMIT APPLICATION
+          </button>
         </form>
       </div>
+      {isSuccessAlert && (
+        <div
+          className={`${styles.alertContainer} ${styles.alertSuccess} ${styles.slideIn}`}
+        >
+          Successfully submitted!
+        </div>
+      )}
+      {isErrorAlert && (
+        <div
+          className={`${styles.alertContainer} ${styles.alertError} ${styles.slideIn}`}
+        >
+          {errormsg}
+        </div>
+      )}
     </div>
   );
 }
