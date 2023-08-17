80e5062f84be892cb860689ddb9dcad2cae51556
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-08-17 10:45:21 +0530

    fix

--- src/pages/Career/Pages/AllJobs/Alljobs.jsx
+++ src/pages/Career/Pages/AllJobs/Alljobs.jsx
@@ -1,36 +1,45 @@
-import styles from "./style.module.css"
+import styles from "./style.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
-import React from 'react'
+import React from "react";
+import AnimatedComponent from "../../../../components/AnimatedComponent";
 
 function Alljobs() {
   return (
-    <div className={styles.main}>
+    <AnimatedComponent>
+      <div className={styles.main}>
         <div className={styles.container}>
-            <h1>Career at HARAAY</h1>
-            <p>Open Position <FontAwesomeIcon icon={faArrowRight} className={styles.arrowright} /></p>
-            <div className={styles.jobs}>
-                <p>UI/UX Designer</p>
-            </div>
-            <div className={styles.jobs}>
-                <p>Fron-End Developer</p>
-            </div>
-            <div className={styles.jobs}>
-                <p>Back-End Developer</p>
-            </div>
-            <div className={styles.jobs}>
-                <p>Visual Designer</p>
-            </div>
-            <div className={styles.jobs}>
-                <p>Graphic Designer</p>
-            </div>
-            <a href="/applynow">
-              <button className={styles.button}>Apply Now</button>
-            </a>
+          <h1>Career at HARAAY</h1>
+          <p>
+            Open Position{" "}
+            <FontAwesomeIcon
+              icon={faArrowRight}
+              className={styles.arrowright}
+            />
+          </p>
+          <div className={styles.jobs}>
+            <p>UI/UX Designer</p>
+          </div>
+          <div className={styles.jobs}>
+            <p>Fron-End Developer</p>
+          </div>
+          <div className={styles.jobs}>
+            <p>Back-End Developer</p>
+          </div>
+          <div className={styles.jobs}>
+            <p>Visual Designer</p>
+          </div>
+          <div className={styles.jobs}>
+            <p>Graphic Designer</p>
+          </div>
+          <a href="/applynow">
+            <button className={styles.button}>Apply Now</button>
+          </a>
         </div>
-    </div>
-  )
+      </div>
+    </AnimatedComponent>
+  );
 }
 
-export default Alljobs
+export default Alljobs;
--- src/pages/Career/Pages/ApplyNow/ApplyNow.jsx
+++ src/pages/Career/Pages/ApplyNow/ApplyNow.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./style.module.css";
+import AnimatedComponent from "../../../../components/AnimatedComponent";
 
 function ApplyNow() {
   const [firstName, setFirstName] = useState("");
@@ -12,7 +13,9 @@ function ApplyNow() {
   const [desc, setDesc] = useState("");
   const [isSuccessAlert, setIsSuccessAlert] = useState(false);
   const [isErrorAlert, setIsErrorAlert] = useState(false);
-  const [errormsg, setErrormsg] = useState("Submission failed. Please try again.")
+  const [errormsg, setErrormsg] = useState(
+    "Submission failed. Please try again."
+  );
 
   const navigate = useNavigate();
 
@@ -46,95 +49,97 @@ function ApplyNow() {
           }, 1000);
         }, 2000);
       } else {
-        setIsErrorAlert(true)
+        setIsErrorAlert(true);
         setTimeout(() => {
           setIsErrorAlert(false);
         }, 2000);
       }
     } catch (error) {
-      setErrormsg("Network Error!")
+      setErrormsg("Network Error!");
     }
   };
 
   return (
-    <div className={styles.main}>
-      <div className={styles.container}>
-        <form>
-          <div className={styles.name}>
+    <AnimatedComponent>
+      <div className={styles.main}>
+        <div className={styles.container}>
+          <form>
+            <div className={styles.name}>
+              <input
+                type="text"
+                required
+                placeholder="First Name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+              <input
+                type="text"
+                required
+                placeholder="Last Name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
             <input
-              type="text"
+              type="email"
               required
-              placeholder="First Name"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <input
               type="text"
               required
-              placeholder="Last Name"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
+              placeholder="Number"
+              value={number}
+              onChange={(e) => setNumber(e.target.value)}
+              pattern="[0-9()-\s]+"
             />
+            <div className={styles.resume}>
+              <div className={styles.resumename}>Resume/CV</div>
+              <input
+                type="url"
+                value={resume}
+                onChange={(e) => setResume(e.target.value)}
+                placeholder="Upload on Drive and provide link"
+                required
+              />
+            </div>
+            <input
+              type="url"
+              required
+              placeholder="Portfolio Link"
+              value={portfolio}
+              onChange={(e) => setPortfolio(e.target.value)}
+            />
+            <textarea
+              rows="7"
+              cols="10"
+              placeholder="Personal Note"
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
+            />
+            <button className={styles.button} onClick={submitData}>
+              SUBMIT APPLICATION
+            </button>
+          </form>
+        </div>
+        {isSuccessAlert && (
+          <div
+            className={`${styles.alertContainer} ${styles.alertSuccess} ${styles.slideIn}`}
+          >
+            Successfully submitted!
           </div>
-          <input
-            type="email"
-            required
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="text"
-            required
-            placeholder="Number"
-            value={number}
-            onChange={(e) => setNumber(e.target.value)}
-            pattern="[0-9()-\s]+"
-          />
-          <div className={styles.resume}>
-            <div className={styles.resumename}>Resume/CV</div>
-            <input
-              type="text"
-              value={resume}
-              onChange={(e) => setResume(e.target.value)}
-              placeholder="Upload on Google Drive and provide link"
-              required
-            />
+        )}
+        {isErrorAlert && (
+          <div
+            className={`${styles.alertContainer} ${styles.alertError} ${styles.slideIn}`}
+          >
+            {errormsg}
           </div>
-          <input
-            type="text"
-            required
-            placeholder="Portfolio Link"
-            value={portfolio}
-            onChange={(e) => setPortfolio(e.target.value)}
-          />
-          <textarea
-            rows="7"
-            cols="10"
-            placeholder="Personal Note"
-            value={desc}
-            onChange={(e) => setDesc(e.target.value)}
-          />
-          <button className={styles.button} onClick={submitData}>
-            SUBMIT APPLICATION
-          </button>
-        </form>
+        )}
       </div>
-      {isSuccessAlert && (
-        <div
-          className={`${styles.alertContainer} ${styles.alertSuccess} ${styles.slideIn}`}
-        >
-          Successfully submitted!
-        </div>
-      )}
-      {isErrorAlert && (
-        <div
-          className={`${styles.alertContainer} ${styles.alertError} ${styles.slideIn}`}
-        >
-          {errormsg}
-        </div>
-      )}
-    </div>
+    </AnimatedComponent>
   );
 }
 
