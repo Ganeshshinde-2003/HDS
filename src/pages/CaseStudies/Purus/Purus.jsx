9e297bb71d75ca4194da2b8d56f9ab40f6acdc79
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-10-09 20:09:42 +0530

    created the purus case study

--- src/pages/CaseStudies/Purus/Purus.jsx
+++ src/pages/CaseStudies/Purus/Purus.jsx
@@ -1,14 +1,23 @@
 import styles from "./style.module.css";
 import React from "react";
 import AnimatedComponent from "../../../components/AnimatedComponent";
-import img1 from "../../../assets/casestudies/purus/2 33.png";
 import img2 from "../../../assets/casestudies/purus/Artboard 10@4x-8.png";
 import img4 from "../../../assets/casestudies/purus/Artboard 8@4x-8.png";
 import img5 from "../../../assets/casestudies/purus/Artboard 9@4x-8.png";
-import img6 from "../../../assets/casestudies/purus/Stationery Set 02 Standard Mockup 1 new.png";
 import img7 from "../../../assets/casestudies/toot/TOOT 1.png";
-import img8 from "../../../assets/casestudies/purus/11 (1)-min 1.png";
-import img9 from "../../../assets/casestudies/purus/12 (1)-min 1.png";
+import img10 from "../../../assets/casestudies/purus/caed1.png";
+import img11 from "../../../assets/casestudies/purus/Artboard 9@4x-100.png";
+import img12 from "../../../assets/casestudies/purus/Artboard 4@4x-100.png";
+import img13 from "../../../assets/casestudies/purus/Artboard 10@4x-100.png";
+import img14 from "../../../assets/casestudies/purus/op.png";
+import img15 from "../../../assets/casestudies/purus/Artboard 6@4x-100.png";
+import img16 from "../../../assets/casestudies/purus/19 1.png";
+import si1 from "../../../assets/casestudies/purus/9 4.png";
+import si2 from "../../../assets/casestudies/purus/14 1.png";
+import si3 from "../../../assets/casestudies/purus/11 370.png";
+import si4 from "../../../assets/casestudies/purus/12 (1) 1.png";
+import si5 from "../../../assets/casestudies/purus/17 1.png";
+import si6 from "../../../assets/casestudies/purus/puru 2 (1) 1.png";
 import videoFile from "../../../assets/Comp 2.MP4";
 import { Link } from "react-router-dom";
 
@@ -51,19 +60,71 @@ function Purus() {
         <section className={styles.images2}>
           <img src={img2} alt="PURUS" />
         </section>
+        <section className={styles.images}>
+          <img src={img10} alt="PURUS" />
+        </section>
         <section className={styles.image}>
           <img src={img4} alt="PURUS" />
           <img src={img5} alt="PURUS" />
         </section>
         <section className={styles.images}>
-          <img src={img1} alt="PURUS" />
-        </section>
-        <section className={styles.image}>
-          <img src={img8} alt="PURUS" />
-          <img src={img9} alt="PURUS" />
+          <img src={img11} alt="PURUS" />
         </section>
         <section className={styles.images}>
-          <img src={img6} alt="PURUS" />
+          <img src={img12} alt="PURUS" />
+        </section>
+        <section className={styles.image}>
+          <img src={img13} alt="PURUS" />
+          <img src={img14} alt="PURUS" />
+        </section>
+        <section className={styles.images}>
+          <img src={img15} alt="PURUS" />
+        </section>
+        <div className={styles.faqsection}>
+          <div className={`${styles.faqiamge} ${styles.text}`}>
+            <p>Packaging</p>
+            <p>
+              The Packaging design for puru’s is inspired by playfullness with
+              variations for the different categories of sustainable products.
+              With the help of Cinema 4D, each packaging has a distinct thought
+              process behind it.
+              <br />
+              <br />
+              The designs of the packaging represents the products inside them.
+              from minimal formals for the adults to scribble art for kids, we
+              value and understand the needs
+              <br />
+              <br />
+              The design theme is purely based on the idea to make this
+              packaging more engaging for kids. The colour pencil’s on the
+              packaging represents the colour pencil’s inside the packaging.
+              here, the back of this packaging is a cutout of an oragami for
+              kids to colour, cut and play with it.
+            </p>
+          </div>
+          <div className={styles.faqs}>
+            <div className={styles.faq1}>
+              <img src={si1} alt="PURUS" />
+            </div>
+            <div className={styles.faq1}>
+              <img src={si2} alt="PURUS" />
+            </div>
+            <div className={styles.faq1}>
+              <img src={si3} alt="PURUS" />
+            </div>
+            <div className={styles.faq1}>
+              <img src={si4} alt="PURUS" />
+            </div>
+            <div className={styles.faq1}>
+              <img src={si5} alt="PURUS" />
+            </div>
+            <div className={styles.faq1}>
+              <img src={si6} alt="PURUS" />
+            </div>
+          </div>
+        </div>
+        <section className={styles.images}>
+          <img src={img16} alt="PURUS" />
         </section>
         <div className={styles.headings}>
           <p>Toot</p>
