9a9df6ad483bb8f25c43a05a3b66cffc8585e231
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-12-21 23:14:27 +0530

    Created the brand identity:

--- src/pages/Service/BrandIdentity.jsx
+++ src/pages/Service/BrandIdentity.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from "react";
+import styles from "./style.module.css";
 
 const BrandIdentity = () => {
   return (
-    <div>BrandIdentity</div>
-  )
-}
+    <div className={styles.brandContainer}>
+      <p className={styles.brandHeading}>BRAND IDENTITY</p>
+      <p className={styles.brandDisc1}>
+        In the marketplace, it's important for your brand to be remembered the
+        way you intended it to. Your brand is the sum of the consumers
+        perceptions, opinions and feelings, and that's why as a brand owner,
+        branding is how to take control of your own ste and message. As one of
+        the most creative branding companies in India, we work with you to
+        carefully craft a compelling brand is always aligned with your vision,
+        and remembered as distinct.
+      </p>
+      <p className={styles.brandDisc2}>
+        To create a Comprehensive brand identity, I need to understand the core
+        of your brand.
+        <br />
+        <br />
+        • What is the name of your brand?
+        <br />
+        • What products or services do you offer?
+        <br />
+        • Who is your target audience?
+        <br />
+        • What are your brand values?
+        <br />
+        • What makes your brand unique?
+        <br />
+        • What personality do you want your brand to have?
+        <br />
+        • What are your brand goals?
+        <br />
+        <br />
+        Once we have a better understanding of your brand, we start to develop a
+        brand identity that includes the following elements:
+      </p>
+      <div className={styles.brandDisc3}>
+        <p className={styles.brandDisc3Head}>Brand name and tagline: </p>
+        <p className={styles.brandDisc3Disc}>
+          A memorable and unique name and tagline that captures . the essence of
+          your brand.
+        </p>
+      </div>
+      <div className={styles.brandDisc3}>
+        <p className={styles.brandDisc3Head}>Logo and visual identity: </p>
+        <p className={styles.brandDisc3Disc}>
+          A visually appealing logo and overall design aesthetic that reflects
+          your brand personality and values.
+        </p>
+      </div>
+      <div className={styles.brandDisc3}>
+        <p className={styles.brandDisc3Head}>Brand voice and tone: </p>
+        <p className={styles.brandDisc3Disc}>
+          The way you communicate with your audience, using language that is
+          consistent with your brand personality.
+        </p>
+      </div>
+      <div className={styles.brandDisc3}>
+        <p className={styles.brandDisc3Head}>Brand story: </p>
+        <p className={styles.brandDisc3Disc}>
+          The narrative that tells your brand's history, mission, and values
+        </p>
+      </div>
+      <p className={styles.footerHead}>
+        Want to get your brand identity done? Let’s talk about it!!
+      </p>
+    </div>
+  );
+};
 
-export default BrandIdentity
+export default BrandIdentity;
